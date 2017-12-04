abee114a73b0e225eae0b6c740981955ab142366
Anup Arakkal <anup.anup2001@gmail.com>
2017-12-05 00:45:02 +0530

    Added bits and pieces of oauth library

--- src/app/goalplanner/goalplanner.component.ts
+++ src/app/goalplanner/goalplanner.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Goal, SIP, FundType } from './goal.model';
+import { HttpClient } from '@angular/common/http';
 
+declare var gapi: any;
 @Component({
   selector: 'app-goalplanner',
   templateUrl: './goalplanner.component.html',
@@ -8,7 +10,11 @@ import { Goal, SIP, FundType } from './goal.model';
 })
 export class GoalplannerComponent implements OnInit {
   goals: Goal[] = new Array<Goal>();
-  constructor() {
+  message: String;
+  googleUser: any;
+
+  constructor(ngZone: NgZone, private http: HttpClient) {
+    window['onSignIn'] = (user) => ngZone.run(() => this.onSignIn(user));
     const goal = new Goal('Emergency Fund', 12 + Math.round(Math.random() * 36),
     40000 + Math.round(Math.random() * 40000));
     goal.currentSIPs.push(new SIP(0, 0, 0, FundType.Equity));
@@ -26,5 +32,37 @@ export class GoalplannerComponent implements OnInit {
     goal.goalSIP = new SIP(0, 0);
     this.goals.push(goal);
   }
+
+  onSignIn(googleUser) {
+    this.googleUser = googleUser;
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  logOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+
+    if (auth2.isSignedIn.get()) {
+      const profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+
+    auth2.signOut().then(() => console.log('User logged out'));
+  }
+
+  validateUser() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    const id_token = this.googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+    this.http.post('/api/validate_user', {'id_token': id_token}).subscribe( data => console.log(data));
+ }
 
 }
