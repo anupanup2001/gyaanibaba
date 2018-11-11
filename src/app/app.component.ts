2c3d871722f070d2bc949bfbfe0a4507d38d5840
Anup Arakkal <anup.anup2001@gmail.com>
2018-11-11 10:17:47 +0530

    Updated to Angular 7.0.3 fixes #32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-import { ViewEncapsulation } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // tslint:disable-next-line:one-line
-  currentSelection = 'home';
-  JSON: any;
-  constructor(private http: HttpClient, public afAuth: AngularFireAuth) {
-    this.JSON = JSON;
-  }
-  title = 'hello paisagyaan!';
-
-  selectTab(tab: string) {
-    this.currentSelection = tab;
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-  }
-  logout() {
-    this.afAuth.auth.signOut();
-  }
+  title = 'paisagyaan';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { GoalplannerComponent } from './goalplanner/goalplanner.component';
-import { GoalComponent } from './goalplanner/goal/goal.component';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import { AppMaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GoalplannerComponent,
-    GoalComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
-    FlexLayoutModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
